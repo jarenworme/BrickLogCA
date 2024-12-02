fa76dcbe3edae4abf3429ae7c009b3852757fbdc
jarenworme <jarenworme@gmail.com>
2024-12-02 16:30:39 -0500

    configured route file for all new files

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,20 +1,22 @@
 import {useRoutes} from "react-router-dom";
 
-import LandingPage from "../pages/LandingPage";
-import Auth from "../pages/Auth";
-import TestPage from "../pages/TestPage";
 import ProtectedRoute from "./ProtectedRoute";
-
-// import SetList from "../pages/SetList";
-// import SetDetail from "../pages/SetDetail";
-// import AddNewSet from "../pages/AddNewSet";
-// import AddNewPiece from "../pages/AddNewPiece";
-// import PieceList from "../pages/PieceList";
-// import EditSet from "../pages/EditSet";
-// import EditPiece from "../pages/EditPiece";
-// import Stats from "../pages/Stats";
-// import OnlineStats from "../pages/OnlineStats";
-// import About from "../pages/About";
+import About from "../pages/About";
+import AddCustomPiece from "../pages/AddCustomPiece";
+import AddCustomSet from "../pages/AddCustomSet";
+import Auth from "../pages/Auth";
+import BrowsePieces from "../pages/BrowsePieces";
+import BrowseSets from "../pages/BrowseSets";
+import EditPiece from "../pages/EditPiece";
+import EditSet from "../pages/EditSet";
+import Home from "../pages/Home";
+import LandingPage from "../pages/LandingPage";
+import OnlineStats from "../pages/OnlineStats";
+import SetDetails from "../pages/SetDetails";
+import Stats from "../pages/Stats";
+import TestPage from "../pages/TestPage";
+import UserPieces from "../pages/UserPieces";
+import UserSets from "../pages/UserSets";
 
 
 export default function Router (){
@@ -23,26 +25,22 @@ export default function Router (){
             path: '/',
             children: [
                 {path: '/', element: <LandingPage />},
+                {path: '/about', element: <About />},
+                {path: '/addCustomPiece', element: (<ProtectedRoute><AddCustomPiece /></ProtectedRoute>)},
+                {path: '/addCustomSet', element: (<ProtectedRoute><AddCustomSet /></ProtectedRoute>)},
                 {path: '/auth', element: <Auth />},
+                {path: '/browsePieces', element: (<ProtectedRoute><BrowsePieces /></ProtectedRoute>)},
+                {path: '/browseSets', element: (<ProtectedRoute><BrowseSets /></ProtectedRoute>)},
+                {path: '/editPiece', element: (<ProtectedRoute><EditPiece /></ProtectedRoute>)},
+                {path: '/editSet', element: (<ProtectedRoute><EditSet /></ProtectedRoute>)},
+                {path: '/home', element: (<ProtectedRoute><Home /></ProtectedRoute>)},
+                {path: '/onlineStats', element: (<ProtectedRoute><OnlineStats /></ProtectedRoute>)},
+                {path: '/setDetails', element: (<ProtectedRoute><SetDetails /></ProtectedRoute>)},
+                {path: '/stats', element: (<ProtectedRoute><Stats /></ProtectedRoute>)},
                 {path: '/test', element: (<ProtectedRoute><TestPage /></ProtectedRoute>)},
+                {path: '/userPieces', element: (<ProtectedRoute><UserPieces /></ProtectedRoute>)},
+                {path: '/userSets', element: (<ProtectedRoute><UserSets /></ProtectedRoute>)},
             ]
-            // children: [
-            //     {path: '/', element: <LandingPage />},
-            //     {path: '/home', element: <Home />},
-            //     {path: '/register', element: <Register />},
-            //     {path: '/login', element: <Login />},
-            //     {path: "/logout", element: <Logout />},
-            //     {path: '/SetList', element: <SetList />},
-            //     {path: '/PieceList', element: <PieceList />},
-            //     {path: '/SetDetail/page/:prevPage/set/:setId', element: <SetDetail />},
-            //     {path: '/AddNewSet', element: <AddNewSet />},
-            //     {path: '/AddNewPiece/page/:prevPage/set/:setId/data/:missing', element: <AddNewPiece />},
-            //     {path: '/EditSet/page/:prevPage/set/:setId', element: <EditSet />},
-            //     {path: '/EditPiece/page/:prevPage/set/:setId/piece/:pieceId/thr/:threshold', element: <EditPiece />},
-            //     {path: "/Stats", element: <Stats />},
-            //     {path: "/OnlineStats", element: <OnlineStats />},
-            //     {path: "/About", element: <About />},
-            // ]
         },
     ]);
 }
